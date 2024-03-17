34b0b5e18899295489ea201e2a00517f2c3f00fe
Mustafa Kumshi <102613277+mustafakumshi@users.noreply.github.com>
2024-03-17 18:29:15 +0000

    Added Tab Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  let tabContent = "Please click a button";
+
+  function handleSelect(selectedButton){
+    //selectedButton = "components", "jsx", "props", "state"
+    tabContent = selectedButton;
+    console.log(tabContent);
+  }
+
+  console.log("APP COMPONENT EXECUTING");
+
   return (
     <div>
       <Header />
@@ -15,6 +26,16 @@ function App() {
           <CoreConcept {...CORE_CONCEPTS[2]}/>
           <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")} >Components</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")} >JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")} >Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")} >State</TabButton>
+          </menu>
+          {tabContent}
         </section>
       </main>
     </div>
