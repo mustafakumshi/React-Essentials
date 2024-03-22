79530326608eba702df690641c1781f6a70e82f9
Mustafa Kumshi <102613277+mustafakumshi@users.noreply.github.com>
2024-03-22 17:06:54 +0000

    AAdded import for Examples

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from './data.js';
 
 function App() {
   const [selectedTopic, setSelectedTopic] = useState("please click a button");
